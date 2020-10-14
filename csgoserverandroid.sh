4e76e9bbbfce4d0be6aa88376905e7e0c7e2cdc3
n0x <ide.boschma02@gmail.com>
2020-10-14 20:28:03 +0200

    Add files via upload

---
+++ csgoserverandroid.sh
@@ -0,0 +1,19 @@
+mkdir server
+cd server
+mkdir csgo
+cd csgo
+csgodir=$(pwd)
+cd ..
+mkdir steamcmd
+cd steamcmd
+sudo apt-get install lib32gcc1
+wget  https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
+tar zxvf steamcmd_linux.tar.gz
+rm -r steamcmd_linux.tar.gz
+./steamcmd.sh +quit
+./steamcmd.sh +login anonymous +force_install_dir $csgodir +app_update 740 validate +quit
+cd ..
+cd csgo
+echo './srcds_run -game csgo -port 27015 +maxplayers 8 +exec server.cfg +map de_dust2' > runcsgo.sh
+chmod 777 runcsgo.sh
+./runcsgo.sh
